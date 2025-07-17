408a12492625c83caf4d4375c1a444abc1836622
XOFAA <giovanne.oliv@gmail.com>
2025-07-17 15:48:30 -0400

    alterando rota api

--- src/components/cursos/CardCurso.jsx
+++ src/components/cursos/CardCurso.jsx
@@ -64,7 +64,7 @@ export const CardCurso = ({ curso }) => {
                 },
             }}>
                 <Box sx={{ position: "relative" }}>
-                    <CardMedia component="img" height="160px" image={`http://localhost:3000/${curso.thumbnail}`} alt={curso.titulo} />
+                    <CardMedia component="img" height="160px" image={`https://api.digitaleduca.com.vc/${curso.thumbnail}`} alt={curso.titulo} />
                     <Chip
                         label={curso.level}
                         size="small"
